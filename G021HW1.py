92f0d91fa33dd231b0c5f1f5f2fc37dd312e652f
matehrani <t.mohammadamin@gmail.com>
2023-04-05 14:23:44 +0200

    Implement argparser

--- G021HW1.py
+++ G021HW1.py
@@ -1,5 +1,6 @@
 from pyspark import SparkConf, SparkContext
 from count_triangles import count_triangles
+from argparse import ArgumentParser
 
 def MR_ApproxTCwithNodeColors(rdd, C) -> int:
     pass
@@ -10,4 +11,13 @@ def MR_ApproxTCwithSparkPartitions(rdd, C) -> int:
 
 
 if __name__ == '__main__':
-    pass
+    parser = ArgumentParser(description="BDC - Group 021 - Assignment 1")
+    
+    parser.add_argument('C', type=int, help='Number of colors')
+    parser.add_argument('R', type=int, help='Number of Repetitions')
+    parser.add_argument('FILE', type=str, help='File path')
+    parser.add_argument('-o','--output', required=False, default=None, help='path to save the output there (optional)')
+    
+    args = parser.parse_args()
+
+    
