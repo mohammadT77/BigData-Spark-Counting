83ba904a0ee1417bb209b5f5691dedde91de0b15
matehrani <t.mohammadamin@gmail.com>
2023-04-05 14:10:25 +0200

    Add G021HW1.py

---
+++ G021HW1.py
@@ -0,0 +1,13 @@
+from pyspark import SparkConf, SparkContext
+from count_triangles import count_triangles
+
+def MR_ApproxTCwithNodeColors(rdd, C) -> int:
+    pass
+
+
+def MR_ApproxTCwithSparkPartitions(rdd, C) -> int:
+    pass
+
+
+if __name__ == '__main__':
+    pass
