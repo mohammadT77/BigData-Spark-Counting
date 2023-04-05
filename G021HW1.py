90e2d307de7b7fcca2fb6c580d8a9d857fedabc7
matehrani <t.mohammadamin@gmail.com>
2023-04-05 14:48:04 +0200

    implement hash function

--- G021HW1.py
+++ G021HW1.py
@@ -2,6 +2,15 @@ from pyspark import SparkConf, SparkContext
 from count_triangles import count_triangles
 from argparse import ArgumentParser
 from os.path import isfile
+from random import randint
+
+def h_(c):
+    p = 8191
+    def hash_func(u):
+        a = randint(1, p-1)
+        b = randint(0, p-1)
+        return ((a*u + b) % p) % c
+    return hash_func
 
 def MR_ApproxTCwithNodeColors(rdd, C) -> int:
     pass
@@ -24,15 +33,19 @@ if __name__ == '__main__':
     assert args.R >= 1, "Invalid argument R"
     assert isfile(args.path), "Invalid data file path (argument FILE_PATH)"
     
+    h_c = h_(args.C)
+
     # Spark
     conf = SparkConf().setAppName("BDC: G021HW1")
     sc = SparkContext(conf=conf)
 
     rdd = sc.textFile(args.path, minPartitions=args.C, use_unicode=False)
+    rdd = rdd.map(lambda s: eval(b'('+s+b')')) # Convert edges from string to tuple.
+
+
     print("Dataset =", args.path)
     print("Number of Edges =", rdd.count())
     print("Number of Colors =", args.C)
     print("Number of Repetitions =", args.R)
 
-    rdd = rdd.map(lambda s: eval(b'('+s+b')')) # Convert edges from string to tuple.
 
