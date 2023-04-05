b64752c5573e8b84c4629c6b041411a58f62905a
matehrani <t.mohammadamin@gmail.com>
2023-04-05 14:38:33 +0200

    add assert

--- G021HW1.py
+++ G021HW1.py
@@ -1,6 +1,7 @@
 from pyspark import SparkConf, SparkContext
 from count_triangles import count_triangles
 from argparse import ArgumentParser
+from os.path import isfile
 
 def MR_ApproxTCwithNodeColors(rdd, C) -> int:
     pass
@@ -15,9 +16,19 @@ if __name__ == '__main__':
     
     parser.add_argument('C', type=int, help='Number of colors')
     parser.add_argument('R', type=int, help='Number of Repetitions')
-    parser.add_argument('FILE', type=str, help='File path')
-    parser.add_argument('-o','--output', required=False, default=None, help='path to save the output there (optional)')
+    parser.add_argument('path', metavar="FILE_PATH", type=str, help='Dataset file path')
     
     args = parser.parse_args()
-
     
+    assert args.C >= 1, "Invalid argument C"
+    assert args.R >= 1, "Invalid argument R"
+    assert isfile(args.path), "Invalid data file path (argument FILE_PATH)"
+    
+    # Spark
+    conf = SparkConf().setAppName("BDC: G021HW1")
+    sc = SparkContext(conf=conf)
+
+    rdd = sc.textFile(args.path, minPartitions=args.C, use_unicode=False)
+    print("Number of edges:", rdd.count())
+    
+    print(rdd.collect())
