17142a1cb53c2d461b18bc2776d44836d6501cab
ali <alikhorasani97@gmail.com>
2023-04-12 11:00:27 +0200

    trying to optimize Algorithm 2

--- G021HW1_Ali.py
+++ G021HW1_Ali.py
@@ -60,12 +60,19 @@ def MR_ApproxTCwithNodeColors(edges,c):
                 )
     return triangle_counting
 
-
+global key
+key = -1
 def MR_ApproxTCwithSparkPartitions(edges,c):
-    triangle_counting_random = (edges.groupBy(lambda x: (rand.randint(0,c-1)))#.map(lambda x : (x[0], list(x[1])))
-                         .mapValues(CountTriangles)  # (0,2200) , (1,2100) , (2,2000),3(
-                         .values()
-                         .sum()*c*c
+
+    def add_key(edges):
+        key = key+1
+        return [(key,edges)]
+
+    triangle_counting_random = (edges.glom()#.partitionBy(c)#.groupBy(lambda x: (rand.randint(0,c-1)))#.map(lambda x : (x[0], list(x[1])))
+                         .flatMap(add_key)
+                         #.mapValues(CountTriangles)  # (0,2200) , (1,2100) , (2,2000),3(
+                         #.values()
+                         #.sum()*c*c
                          )
     return triangle_counting_random
 
@@ -93,7 +100,7 @@ def main():
     data_path = sys.argv[3]
     assert os.path.isfile(data_path), "File or folder not found"
     rawData = sc.textFile(data_path).repartition(numPartitions=c).cache()
-    edges = rawData.flatMap(rawData_to_edges)
+    edges = rawData.flatMap(rawData_to_edges).repartition(numPartitions=c).cache()
     #docs.repartition(numPartitions=c)
 
     #setting global variables
@@ -103,14 +110,14 @@ def main():
     avg_running_time1 = 0
     avg_running_time2 = 0
     cur_runtime = 0
-
+    '''
     for i in range(r):
         start_time = time()
         Number_of_triangles.append(MR_ApproxTCwithNodeColors(edges,c))
         cur_runtime = (time() - start_time)*1000
         avg_running_time1+= cur_runtime
     avg_running_time1 = avg_running_time1/r
-
+    '''
     for i in range(r):
         start_time = time()
         Number_of_triangles_spark.append(MR_ApproxTCwithSparkPartitions(edges,c))
@@ -124,13 +131,13 @@ def main():
     print("Number of Colors = "+str(c))
     print("Number of Repetitions = "+str(r))
     
-    print("Number of Triangle in the graph =",statistics.median(Number_of_triangles))
-    print("Runtime = ",avg_running_time1)
+    #print("Number of Triangle in the graph =",statistics.median(Number_of_triangles))
+    #print("Runtime = ",avg_running_time1)
 
 
-    print("Number of Triangle in the graph =", statistics.median(Number_of_triangles_spark))
+    #print("Number of Triangle in the graph =", statistics.median(Number_of_triangles_spark))
     print("Runtime = ", avg_running_time2)
-    #print("Number of Triangle in the graph =", Number_of_triangles_spark[0].collect())#[0].collect())
+    print("Number of Triangle in the graph =", Number_of_triangles_spark[0].collect().__len__())#.collect())
 #setting global variables
 
 if __name__ == '__main__':
